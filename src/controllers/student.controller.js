47f7515c31625bb35b9cd1102b781803a1472359
Mufasael21 <masaboolivier21@gmail.com>
2025-05-07 19:25:01 +0200

    feat:update student endpoint

--- src/controllers/student.controller.js
+++ src/controllers/student.controller.js
@@ -23,3 +23,36 @@ export const getAllStudents = async (req, res) => {
     });
   }
 };
+
+
+export const updateStudentById = async (req,res) =>{
+ 
+  const id =req.params.id
+  try{
+    const checking =await pool.query(`SELECT * FROM students WHERE id=$1`,[id])
+    if(checking.rowCount ===0){
+      res.status(404).json({success:false,message:"user not found"})
+}
+  const {first_name, last_name, student_id, email, date_of_birth, 
+  contact_number, enrollment_date,profile_picture} =req.body
+
+  const update_query ="UPDATE students SET first_name=$1, last_name=$2, student_id=$3, email=$4, date_of_birth=$5, contact_number=$6, enrollment_date=$7,profile_picture=$8 WHERE id=$9";
+  await pool.query(update_query,[first_name,last_name,student_id,email,date_of_birth,contact_number,enrollment_date,profile_picture,id])
+    
+  res.status(200).json({
+    success: true,
+    count: students.rows.length,
+    data: students.rows,
+  });
+  
+
+  }
+  catch (err) {
+    logger.error(err.message);
+    res.status(500).json({
+      success: false,
+      message: `An unexpected error occurred in PUT/STUDENTS, ${err?.message}`,
+    });
+  }
+
+  }
